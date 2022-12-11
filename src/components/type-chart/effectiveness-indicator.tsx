7f668a5835a3c8df7234549376e814086e027a7e
Roy Stewart <roythomasstewart@gmail.com>
2022-12-11 16:08:07 -0500

    Added appearance of the effectiveness indicator

--- src/components/type-chart/effectiveness-indicator.tsx
+++ src/components/type-chart/effectiveness-indicator.tsx
@@ -1,9 +1,20 @@
 import { Effectiveness } from "../../models";
 
+import styles from "./effectiveness-indicator.module.css";
+
 interface EffectivenessIndicatorProps {
     effectiveness: Effectiveness,
 }
 
 export const EffectivenessIndicator = ({ effectiveness }: EffectivenessIndicatorProps) => {
-    return null;
+    switch (effectiveness) {
+        case Effectiveness.NO_EFFECT:
+            return <span className={styles.noEffect}>0</span>;
+        case Effectiveness.NOT_EFFECTIVE:
+            return <span className={styles.notEffective}>1/2</span>;
+        case Effectiveness.NEUTRAL:
+            return <span className={styles.neutral}>1</span>;
+        case Effectiveness.VERY_EFFECTIVE:
+            return <span className={styles.veryEffective}>2</span>;
+    }
 };
