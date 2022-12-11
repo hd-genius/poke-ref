443fa675c2cd220898f87f1abdcf0369ec9d2ca6
Roy Stewart <roythomasstewart@gmail.com>
2022-12-11 10:43:42 -0500

    Added pokemon types

--- src/models/pokemon-type.ts
+++ src/models/pokemon-type.ts
@@ -1,3 +1,20 @@
 export enum PokemonType {
-    
+    NORMAL,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DARK,
+    DRAGON,
+    STEEL,
+    FAIRY,
 }
