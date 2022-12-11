601fb1b7aefdc81d6e64209d53564b9f8d646b22
Roy Stewart <roythomasstewart@gmail.com>
2022-12-11 01:41:39 -0500

    Added uncommited file

---
+++ src/app-router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([]);
+
+export const AppRouter = () => {
+  return (
+    <RouterProvider router={router} />
+  );
+};
