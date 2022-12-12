606e1ad6af309495de412f891c53a1a11793fda0
Roy Stewart <roythomasstewart@gmail.com>
2022-12-12 10:58:11 -0500

    Added TypeIndicator tests

---
+++ src/components/type-indicator/type-indicator.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { TypeIndicator } from "./type-indicator";
+import { PokemonType } from "../../models";
+
+
+describe("TypeIndicator", () => {
+    it("should show the type name when it is not abbreviated", () => {
+        render(<TypeIndicator type={PokemonType.GRASS} />);
+        expect(screen.getByText("grass")).toBeVisible();
+    });
+
+    it("should show the abbreviated name when it is abbreviated", () => {
+        render(<TypeIndicator type={PokemonType.GRASS} isAbbreviated />);
+        expect(screen.getByText("gra")).toBeVisible();
+    });
+
+    it.todo("should pass through the className");
+
+    it.todo("should show the correct styles for the type");
+});
